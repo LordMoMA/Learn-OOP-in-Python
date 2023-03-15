b70ca57ab4c8f279443a3f82b26d4be0f83a17fe
David Lee <lihongyi0512@gmail.com>
2023-03-15 22:37:56 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -332,3 +332,34 @@ def main():
 
 
 main()
+
+'''
+Multiple Objects
+If a class is just a type, then an object is just a value.
+
+You'll hear often that an object is an "instance" of a class. Let's look at what that word means.
+
+In object-oriented programming, an instance is a concrete occurrence of any object... "Instance" is synonymous with "object" as they are each a particular value... "Instance" emphasizes the distinct identity of the object. The creation of an instance is called instantiation.
+
+-- Wikipedia
+
+So for our wall class, I can create three different "instances" of the class. Or in other words, I'll create three separate objects.
+
+south_wall = Wall(1, 2, 3)
+north_wall = Wall(4, 5, 6)
+east_wall = Wall(9, 8, 7)
+
+height = 3
+width = 4
+depth = 5
+In the example above, Wall and Integer are types, and each variable is an instance of one of those types.
+
+Assignment
+Take a look at the Brawler class and the fight function provided. In the main function, create 4 new brawlers with the following stats:
+
+Name: Aragorn. Speed: 4. Strength: 4.
+Name: Gimli. Speed: 2. Strength: 7.
+Name: Legolas. Speed: 7. Strength: 7.
+Name: Frodo. Speed: 3. Strength: 2.
+Then call fight twice. The first fight should be Aragorn vs Gimli. The second will be Legolas vs Frodo.
+'''
