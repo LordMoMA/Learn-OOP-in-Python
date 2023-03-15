f7b2031a877310833c29e84f8a821f0294caad82
David Lee <lihongyi0512@gmail.com>
2023-03-15 22:33:18 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -260,3 +260,19 @@ def main():
 
 
 main()
+
+'''
+Methods vs Functions
+What is a function?
+A function is a piece of code that is called by a name. It can be passed data to operate on via parameters and can optionally return data. All data that is passed to a function is explicitly passed through parameters.
+
+What is a method?
+A method is a piece of code that is called by a name that is associated with an object. Methods and functions are similar but have two key differences.
+
+A method is implicitly passed the object on which it was called. In other words, you won't see all the inputs in the parameter list
+A method can operate on data that is contained within the class. In other words, you won't see all the outputs in the return statement.
+The OOP debate
+Because functions are more explicit, some developers argue that functional programming is better than object-oriented programming. In reality, neither paradigm is "better", and the best developers learn and understand both styles and use them as they see fit.
+
+For example, while methods are more implicit and often make code more difficult to read, they also make it easier to group a program's data and behavior in one place, which can lead to a more organized codebase.
+'''
