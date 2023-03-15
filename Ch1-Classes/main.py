636e272f663ea7d0e587649b9103ab35694e887c
David Lee <lihongyi0512@gmail.com>
2023-03-15 22:33:58 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -276,3 +276,35 @@ Because functions are more explicit, some developers argue that functional progr
 
 For example, while methods are more implicit and often make code more difficult to read, they also make it easier to group a program's data and behavior in one place, which can lead to a more organized codebase.
 '''
+##########################################
+'''
+Constructors (or initializers)
+It's quite rare in the real world to see a class that defines properties in the way we've been doing it.
+
+class Soldier:
+    armor = 2
+    num_weapons = 2
+It's much more practical to use a constructor. In Python, the constructor is the init() method, and it is called when a new object is created.
+
+So, with a constructor, the code would look like this.
+
+class Soldier:
+    def __init__(self):
+        self.armor = 2
+        self.num_weapons = 2
+However, because the constructor is a method, we can now make the starting armor and number of weapons configurable with some parameters.
+
+class Soldier:
+    def __init__(self, armor, num_weapons):
+        self.armor = armor
+        self.num_weapons = num_weapons
+
+soldier = Soldier(5, 10)
+print(soldier.armor)
+# prints "5"
+print(soldier.num_weapons)
+# prints "10"
+Assignment
+Add a constructor to our Wall class. It should take depth, height and width as parameters, and set them as properties. It should also compute an additional property called volume. Volume is the width times height times depth.
+'''
+
