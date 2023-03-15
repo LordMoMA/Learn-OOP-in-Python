26d25f1bdc17bc0a522d7385831b517e82215dc3
David Lee <lihongyi0512@gmail.com>
2023-03-15 22:32:19 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -204,3 +204,59 @@ def main():
 
 
 main()
+
+'''
+Methods can return values
+If a normal function doesn't return anything, it's typically not a very useful function. Methods often don't return anything explicitly because they often mutate the properties of the object instead. That's exactly what we did in the last assignment.
+
+However, they can return values as well!
+
+class Soldier:
+    armor = 2
+    num_weapons = 2
+
+    def get_speed(self):
+        speed = 10
+        speed -= self.armor
+        speed -= self.num_weapons
+        return speed
+
+soldier_one = Soldier()
+print(soldier_one.get_speed())
+# prints "6"
+Assignment
+Add a get_cost() method to your wall class. The cost of a wall is a function of the wall's height and armor:
+
+cost = armor * height
+'''
+class Wall:
+    armor = 10
+    height = 5
+
+    def get_cost(self):
+        return self.armor * self.height
+
+    # -- TEST SUITE, DONT TOUCH BELOW THIS LINE --
+
+    def fortify(self):
+        self.armor *= 2
+
+
+def main():
+    wall = Wall()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+    print("fortifying wall...")
+    wall.fortify()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+    print("fortifying wall...")
+    wall.fortify()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+    print("fortifying wall...")
+    wall.fortify()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+
+main()
