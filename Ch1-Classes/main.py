43a86a183128ff79d6d1285b07cd8dc9346c4f2c
David Lee <lihongyi0512@gmail.com>
2023-03-15 22:14:59 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -71,3 +71,42 @@ def main():
 
 
 main()
+
+'''
+Clean code
+Object-oriented programming and other paradigms like functional programming are all about making code easier to work with and understand. We call code that is easy to work with "clean code".
+
+Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
+
+-- Martin Fowler
+
+Clean code is not
+A way to make your programs run faster
+A way to make your program use less memory
+Necessary to create certain kinds of programs
+Strictly better than non-OOP code
+Clean code is
+Designed to make code easier to work with in many situations
+Something that helps humans model the real world
+A way to make finding and fixing bugs easier
+A way to make new feature development faster
+The best way to stay sane as a software engineer
+
+DRY code
+Take a look at the code we wrote. We started with this:
+
+soldier_one_dps = soldier_one["damage"] * soldier_one["attacks_per_second"]
+soldier_two_dps = soldier_two["damage"] * soldier_two["attacks_per_second"]
+And refactored the code to look like this:
+
+def get_soldier_dps(soldier):
+    return soldier["damage"] * soldier["attacks_per_second"]
+soldier_one_dps = get_soldier_dps(soldier_one)
+soldier_two_dps = get_soldier_dps(soldier_two)
+Don't repeat yourself (DRY)
+We don't want too much of our code doing the same thing. When code is duplicated, it leads to many potential problems. In our example, let's pretend the soldier dictionary changed, and now the key that stores the "damage" value is called dmg.
+
+In the first example, we would need to update two lines of code. In the second example, we only need to make the change in one place.
+
+It's not a big deal when two lines are the same and exist right next to each other. However, imagine if we had done this several hundred times in ten or twenty different code files! All of sudden, it makes a lot of sense to stop repeating yourself and write more reusable functions. We call that DRY code.
+'''
