b1c731ed0a2a6068088a631e3607cff594e974e8
David Lee <lihongyi0512@gmail.com>
2023-03-15 22:22:24 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -180,3 +180,27 @@ object.method()
 Assignment
 Add a fortify() method to your wall class. It should double the current armor property.
 '''
+class Wall:
+    armor = 10
+    height = 5
+
+    def fortify(self):
+        self.armor *= 2
+
+
+# -- TEST SUITE, DONT TOUCH BELOW THIS LINE --
+
+
+def main():
+    wall = Wall()
+    print(wall.armor)
+    print(wall.height)
+    wall.fortify()
+    print(wall.armor)
+    print(wall.height)
+    wall.fortify()
+    print(wall.armor)
+    print(wall.height)
+
+
+main()
