fcb17737724bfbd9b336baf2804a9ac1b96e0885
David Lee <lihongyi0512@gmail.com>
2023-03-15 23:11:00 +0800

    “ch1”

--- Ch1-Classes/main.py
+++ Ch1-Classes/main.py
@@ -509,3 +509,32 @@ In the main() function (that our team isn't responsible for), a line like Dragon
 
 Update the Dragon class. Remove the element class variable and instead use an instance variable that's configurable via the constructor.
 '''
+class Dragon:
+    def __init__(self, element):
+        self.element = element
+
+    def get_breath_damage(self):
+        if self.element == "fire":
+            return 300
+        if self.element == "ice":
+            return 150
+        return 0
+
+
+# -- TEST SUITE, DONT TOUCH BELOW THIS LINE --
+
+
+def main():
+    first_dragon = Dragon("fire")
+    print(
+        f"{first_dragon.element} dragon does {first_dragon.get_breath_damage()} damage"
+    )
+
+    second_dragon = Dragon("ice")
+    Dragon.element = "fire"
+    print(
+        f"{second_dragon.element} dragon does {second_dragon.get_breath_damage()} damage"
+    )
+
+
+main()
