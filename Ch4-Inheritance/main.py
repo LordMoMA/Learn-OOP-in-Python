bb7d134b3198bba9bed9ec40562447314c9b5c42
David Lee <lihongyi0512@gmail.com>
2023-03-16 22:08:03 +0800

    “ch3”

--- Ch4-Inheritance/main.py
+++ Ch4-Inheritance/main.py
@@ -133,3 +133,8 @@ def identify(human):
 
 
 main()
+
+'''
+Inheritance hierarchy quiz
+There is no limit to how deeply we can nest an inheritance tree. For example, a Cat can inherit from an Animal which inherits from Living_Thing. That said, we should always be careful that each time we inherit from a base class that the child is a strict subset of the parent. You should never think to yourself "my child's class needs a couple of the parent's methods, but not these other ones" and still decide to inherit from that parent.
+'''
