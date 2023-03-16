619bc17e04d3c7c72692a649a5db7073c6e0e92a
David Lee <lihongyi0512@gmail.com>
2023-03-16 14:21:55 +0800

    “ch2”

--- Ch2-Encapsulation/main.py
+++ Ch2-Encapsulation/main.py
@@ -140,3 +140,59 @@ Casting a fireball costs 20 mana
 If the wizard doesn't have enough mana, raise the exception {} cannot cast fireball
 Otherwise, {1} casts fireball at {2} where {1} is the caster's name and {2} is the target's name, then make sure the target is "fireballed"
 '''
+class Wizard:
+    def cast_fireball(self, target):
+        if self.__mana < 20:
+            raise Exception(f"{self.name} cannot cast fireball")
+        print(f"{self.name} casts fireball at {target.name}")
+        self.__mana -= 20
+        target.get_fireballed()
+
+    # -- TEST SUITE, DONT TOUCH BELOW THIS LINE --
+
+    def __init__(self, name):
+        self.__mana = 45
+        self.__health = 65
+        self.name = name
+
+    def get_mana(self):
+        return self.__mana
+
+    def get_health(self):
+        return self.__health
+
+    def get_fireballed(self):
+        fireball_damage = 30
+        self.__health -= fireball_damage
+        print(f"{self.name} is hit by a fireball")
+        if self.__health <= 0:
+            print(f"{self.name} is dead")
+
+    def drink_mana_potion(self):
+        print(f"{self.name} drinks a mana potion")
+        self.__mana += 40
+
+
+def main():
+    merlin = Wizard("Merlin")
+    madame_mim = Wizard("Madame Mim")
+
+    while madame_mim.get_health() > 0:
+        if merlin.get_mana() < 10:
+            merlin.drink_mana_potion()
+
+        try:
+            print_status(merlin)
+            print_status(madame_mim)
+            merlin.cast_fireball(madame_mim)
+        except Exception as e:
+            print(e)
+
+
+def print_status(wizard):
+    print(
+        f"{wizard.name} has {wizard.get_health()} health and {wizard.get_mana()} mana"
+    )
+
+
+main()
