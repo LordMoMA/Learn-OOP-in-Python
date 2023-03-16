ecd8254af3f1737dade6993b3946dd5656d73b82
David Lee <lihongyi0512@gmail.com>
2023-03-16 23:06:51 +0800

    “ch3”

--- Ch4-Inheritance/main.py
+++ Ch4-Inheritance/main.py
@@ -325,3 +325,77 @@ Vehicle -> Truck
         -> Boat
         -> Train
 '''
+'''
+Dragons
+In "Age of Dragons", there are Orcs, Humans, Goblins, Dragons, etc. All of those different creatures are called "units". At the moment, the only thing specific to a unit is that it has a position on the game map and a name.
+
+Dragons, a specific type of unit, can breathe fire in a large area dealing damage to any units that are touched by its fiery blaze.
+
+The game grid
+Our game map is just a Cartesian plane.
+
+cartesian
+
+Assignment
+Complete the unit's in_area method and the dragon's breathe_fire method.
+
+in_area
+If the unit's position is in the rectangle defined by the parameters, return True. Otherwise, return False. x_1 and y_1 make up the bottom-left point of the rectangle. x_2 and y_2 define the top-right point.
+
+breathe_fire
+This method causes the dragon to breathe a swath of fire in the target area. The target area is centered at (x,y). The area stretches for __fire_range in either direction inclusively.
+
+For each unit in the units array, print {} is hit by the fire if the unit is within the blast, where {} is the name of the unit.
+
+Example of fire breath hitting a unit
+breath hitting unit
+'''
+
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x_1, y_1, x_2, y_2):
+        return x_1 <= self.pos_x <= x_2 and y_1 <= self.pos_y <= y_2
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.__fire_range = fire_range
+
+    def breathe_fire(self, x, y, units):
+        x1 = x - self.__fire_range
+        x2 = x + self.__fire_range
+        y1 = y - self.__fire_range
+        y2 = y + self.__fire_range
+        for unit in units:
+            if unit.in_area(x1, y1, x2, y2):
+                print(f"{unit.name} is hit by the fire")
+
+
+# -- TEST SUITE, DONT TOUCH BELOW THIS LINE --
+
+
+def main():
+    print("Creating an army of generic units")
+    units = [
+        Unit("Yvor", 1, 0),
+        Unit("Nicholas", 0, 1),
+        Unit("Eoin", 2, 0),
+        Unit("Cian", 3, 3),
+        Unit("Andrew", -1, 4),
+        Unit("Baran", -6, 5),
+        Unit("Carbry", 2, 1),
+    ]
+    for unit in units:
+        print(f"creating {unit.name} at position {unit.pos_x} {unit.pos_y}")
+
+    smaug = Dragon("Smaug", 6, 6, 2)
+    print(f"{smaug.name} breathes fire at position 1,1")
+    smaug.breathe_fire(1, 1, units)
+
+
+main()
